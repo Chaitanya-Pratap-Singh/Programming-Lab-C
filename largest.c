79f257c9789d1c8b60f43ac52a266ee33545455e
Chaitanya Pratap Singh <chaitanyapratapsingh24@gmail.com>
2022-10-26 20:52:28 +0530

    new updates

--- largest.c
+++ largest.c
@@ -1,10 +1,12 @@
-WAP to find the largest number using ternary operator. Enter the numbers through keyboard.
+// WAP to find the largest number using ternary operator. Enter the numbers through keyboard.
 
 #include <stdio.h>
 void main()
 {
-    int a,b;
+    int a,b,larg;
     printf("Enter two numbers :");
     scanf("%d %d",&a,&b);
+    larg = a>b?a:b;
+    printf("largest number is : %d",larg); 
     
 }
--- swapvar.c
+++ swapvar.c
@@ -1,6 +1,6 @@
-WAP to swap two integer numbers without using third variable.
+// WAP to swap two integer numbers without using third variable.
 #include<stdio.h>  
- int main()    
+void main()    
 {    
 int a, b; 
 printf("Enter two numbers");
@@ -9,5 +9,4 @@ a=a+b;//a=30 (10+20)
 b=a-b;//b=10 (30-20)    
 a=a-b;//a=20 (30-10)    
 printf("\nAfter swap a=%d b=%d",a,b);    
-return 0;  
 }   
